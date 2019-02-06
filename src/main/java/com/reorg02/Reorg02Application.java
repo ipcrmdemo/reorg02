35545d15e24acece72d008175fea9e78e2196325
Matt Cadorette <mcadorette@gmail.com>
2019-02-06 09:25:17 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/reorg02/Reorg02Application.java
@@ -0,0 +1,12 @@
+package com.reorg02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reorg02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reorg02Application.class, args);
+	}
+}
